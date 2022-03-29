3f00f08fabde26ef79dc7e7cb61783e3f7e70cbd
omarsalem7 <omarsalem0721@gmail.com>
2022-03-29 11:27:08 +0200

    add column

--- schema.sql
+++ schema.sql
@@ -9,3 +9,6 @@ CREATE TABLE animals(
    weight_kg DECIMAL NOT NULL,
    PRIMARY KEY(id)
 );
+
+--Add a column species of type string to your animals table.
+ALTER TABLE animals ADD species VARCHAR(250);
