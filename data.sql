630d5b24e25fdaeecc14ad3bb42aaad0e1e2dd1b
omarsalem7 <omarsalem0721@gmail.com>
2022-03-30 15:15:52 +0200

    answer query questions

--- data.sql
+++ data.sql
@@ -151,6 +151,26 @@ UPDATE animals
   WHERE species_id IS NULL;
   
 
+-- Modify your inserted animals to include owner information (owner_id): 
+UPDATE animals
+  SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+  WHERE name LIKE 'Agumon';
+
+UPDATE animals
+  SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+  WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+  SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+  WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+  SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+  WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+  SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+  WHERE name IN ('Angemon', 'Boarmon');
 
 
 
