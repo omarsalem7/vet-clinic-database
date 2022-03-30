3b5ca8ed70907d9dc439376781dc40e61e1d5236
omarsalem7 <omarsalem0721@gmail.com>
2022-03-30 13:09:01 +0200

    make mutiple queries in data and add forign keys

--- data.sql
+++ data.sql
@@ -85,6 +85,71 @@ ROLLBACK TO save_point1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
 
+-- inser data in owners table
+
+INSERT INTO owners(
+  full_name,
+  age
+) VALUES(
+  'Sam Smith',
+  34
+);
+
+INSERT INTO owners(
+  full_name,
+  age
+) VALUES(
+  'Jennifer Orwell',
+  19
+);
+
+INSERT INTO owners(
+  full_name,
+  age
+) VALUES(
+  'Bob',
+  45
+);
+
+INSERT INTO owners(
+  full_name,
+  age
+) VALUES(
+  'Melody Pond',
+  77
+);
+
+INSERT INTO owners(
+  full_name,
+  age
+) VALUES(
+  'Dean Winchester',
+  14
+);
+
+INSERT INTO owners(
+  full_name,
+  age
+) VALUES(
+  'Jodie Whittaker',
+  38
+);
+
+-- insert data in species table
+INSERT INTO species(name) VALUES ('Pokemon');
+INSERT INTO species(name) VALUES ('Digimon');
+
+
+-- Modify inserted animals so it includes the species_id value
+UPDATE animals
+  SET species_id = (SELECT id FROM species WHERE name = 'Digimon')
+  WHERE name LIKE '%mon';
+
+-- All other animals are Pokemon 
+UPDATE animals
+  SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+  WHERE species_id IS NULL;
+  
 
 
 
